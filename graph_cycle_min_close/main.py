fd93b219d2a61311b57ecf1aca6116720afeb94e
Olizko Stephan <127399395+StephanOlizko@users.noreply.github.com>
2024-02-29 18:26:22 +0300

    Ready to make modify

--- graph_cycle_min_close/main.py
+++ graph_cycle_min_close/main.py
@@ -5,68 +5,31 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import numpy as np
 
+
 def finish():
     root.destroy() 
     print("Закрытие приложения")
 
-root = tk.Tk()
-root.geometry('1200x600')
-root.protocol("WM_DELETE_WINDOW", finish)
-root.resizable(False, False)
 
-frame1 = tk.Frame(root, bd=2, relief='groove', width=200)
-#frame2 = tk.Frame(root, bd=2, relief='groove', width=10)
-frame2 = tk.Frame(root, bd=2, relief='groove')
-#frame4 = tk.Frame(root, bd=2, relief='groove', width=10)
-frame3 = tk.Frame(root, bd=2, relief='groove', width=100)
-
-frame1.grid(row=0, column=0, sticky='nsew')
-frame2.grid(row=1, column=0, sticky='nsew')
-frame3.grid(row=0, column=2, rowspan=2, sticky='nsew')
-
-root.grid_rowconfigure(0, weight=1)
-root.grid_rowconfigure(1, weight=1)
-root.grid_columnconfigure(0, weight=3)
-root.grid_columnconfigure(1, weight=1)
-root.grid_columnconfigure(2, weight=3)
-
-def nearest_neighbor_hamiltonian_cycle(graph):
-    
-    #Вывод информации о ребрах графа и их весах
-    for edge in graph.edges:
-        print('Ребро:', edge, 'Вес:', graph[edge[0]][edge[1]]['weight'])
-
-    # Создаем пустой граф для хранения кратчайшего гамильтонова цикла
+def nearest_neighbor_hamiltonian_cycle(graph, canvas, pos):
     hamiltonian_cycle = nx.DiGraph()
-
-    # Выбираем случайную начальную вершину
     current_node = np.random.choice(list(graph.nodes()))
 
-    # Список для отслеживания посещенных вершин
     visited_nodes = [current_node]
 
-    # Пока есть непосещенные вершины
     while len(visited_nodes) < len(graph.nodes()):
-        # Находим ближайшую непосещенную вершину к текущей
         nearest_neighbor = min(graph[current_node], key=lambda x: graph[current_node][x]['weight'])
-        # Добавляем ребро кратчайшего пути к новой вершине в подграф
         hamiltonian_cycle.add_edge(current_node, nearest_neighbor, weight=graph[current_node][nearest_neighbor]['weight'])
-        # Переходим к следующей вершине
         current_node = nearest_neighbor
-        # Отмечаем вершину как посещенную
         visited_nodes.append(current_node)
 
-    # Добавляем ребро от последней вершины к первой если оно существует иначе выводим сообщение об ошибке
     if graph.has_edge(current_node, visited_nodes[0]):
         hamiltonian_cycle.add_edge(current_node, visited_nodes[0], weight=graph[current_node][visited_nodes[0]]['weight'])
     else:
-        print('Гамильтонов цикл не существует')
-        #Запись информации о гамильтоновом цикле в текстовое поле
         text1.delete(1.0, tk.END)
         text1.insert(tk.END, 'Гамильтонов цикл не существует')
         return None
 
-    #Запись информации о гамильтоновом цикле в текстовое поле
     text1.delete(1.0, tk.END)
     text1.insert(tk.END, 'Гамильтонов цикл: ')
     text1.insert(tk.END, hamiltonian_cycle.edges())
@@ -75,18 +38,23 @@ def nearest_neighbor_hamiltonian_cycle(graph):
     
     lenght = 0
     for edge in hamiltonian_cycle.edges:
-        print('Ребро:', edge, 'Вес:', hamiltonian_cycle[edge[0]][edge[1]]['weight'])
         lenght += hamiltonian_cycle[edge[0]][edge[1]]['weight']
 
     text1.insert(tk.END, lenght)
     text1.insert(tk.END, '\n')
 
+    for node in hamiltonian_cycle.nodes:
+        x, y = pos[node]
+        canvas.create_oval(x-10, y-10, x+10, y+10, fill='red')
+        canvas.create_text(x, y, text=str(node))
+
+    for edge in hamiltonian_cycle.edges:
+        x1, y1 = pos[edge[0]]
+        x2, y2 = pos[edge[1]]
+        canvas.create_line(x1, y1, x2, y2, arrow=tk.LAST)
+
     return hamiltonian_cycle
 
-G1 = nx.DiGraph()
-
-node_id = None
-node_coords = None
 
 def edit(event):
     row_id = tree.focus()
@@ -102,7 +70,6 @@ def edit(event):
     edit_entry.bind('<Return>', save_edit)
     edit_entry.place(x=event.x, y=event.y)
 
-    #Обновление данных в графе
     G1.remove_edge(tree.item(row_id)['values'][0], tree.item(row_id)['values'][1])
 
     source = tree.item(row_id)['values'][0]
@@ -114,9 +81,7 @@ def edit(event):
 
 
 def update_tree():
-    #tree.delete(*tree.get_children())
     for edge in G1.edges:
-        #Проверка наличия ребра c данными вершинами в таблице
         source = edge[0]
         target = edge[1]
         flg = False
@@ -131,11 +96,16 @@ def update_tree():
             tree.insert('', 'end', values=(source, target, round(lenght, 2), weight))
 
 def add_node(event):
+    global pos
+
     node_id = len(G1.nodes) + 1
     G1.add_node(node_id, pos=(event.x, event.y))
     canvas1.create_oval(event.x-10, event.y-10, event.x+10, event.y+10, fill='red')
     canvas1.create_text(event.x, event.y, text=str(node_id))
     update_tree()
+
+    node_coords = (event.x, event.y)
+    pos[node_id] = node_coords
 
 
 def add_edge_on_right_click(event):
@@ -159,19 +129,43 @@ def add_edge_on_right_click(event):
                     update_tree()
                     break
                 else:
-                    #Добавляем направленное ребро в граф
                     lenght = ((G1.nodes[node_id]['pos'][0] - G1.nodes[node]['pos'][0])**2 + (G1.nodes[node_id]['pos'][1] - G1.nodes[node]['pos'][1])**2)**0.5
                     weight = 1
                     G1.add_edge(node_id, node, weight=lenght, label=weight)
 
                     x1, y1 = G1.nodes[node_id]['pos']
                     x2, y2 = G1.nodes[node]['pos']
-                    #Рисуем направленное ребро из x1, y1 в x2, y2
+
                     canvas1.create_line(x1, y1, x2, y2, arrow=tk.LAST)
                     node_id = None
                     update_tree()
                     break
 
+
+root = tk.Tk()
+root.geometry('1200x600')
+root.protocol("WM_DELETE_WINDOW", finish)
+root.resizable(False, False)
+
+frame1 = tk.Frame(root, bd=2, relief='groove', width=200)
+frame2 = tk.Frame(root, bd=2, relief='groove')
+frame3 = tk.Frame(root, bd=2, relief='groove', width=100)
+
+frame1.grid(row=0, column=0, sticky='nsew')
+frame2.grid(row=1, column=0, sticky='nsew')
+frame3.grid(row=0, column=2, rowspan=2, sticky='nsew')
+
+root.grid_rowconfigure(0, weight=1)
+root.grid_rowconfigure(1, weight=1)
+root.grid_columnconfigure(0, weight=3)
+root.grid_columnconfigure(1, weight=1)
+root.grid_columnconfigure(2, weight=3)
+
+G1 = nx.DiGraph()
+pos = {}
+
+node_id = None
+node_coords = None
 
 canvas1 = tk.Canvas(root, width=400, height=400, bg='white', bd=2, relief='groove')
 canvas1.grid(row=0, column=1, sticky='nsew')
@@ -179,11 +173,9 @@ canvas1.grid(row=0, column=1, sticky='nsew')
 canvas1.bind('<Button-1>', add_node)
 canvas1.bind('<Button-3>', add_edge_on_right_click)
 
-
 canvas2 = tk.Canvas(root, width=400, height=400, bg='white', bd=2, relief='groove')
 canvas2.grid(row=1, column=1, sticky='nsew')
 
-#таблица ребер графа G1 на фрейме frame5
 tree = ttk.Treeview(frame3, columns=('source', 'target', 'lenght', 'weight'), height=15, show='headings')
 tree.column('source', width=100, anchor='center')
 tree.column('target', width=100, anchor='center')
@@ -197,11 +189,9 @@ tree.heading('weight', text='Вес')
 tree.bind('<Double-1>', edit)
 tree.pack(fill='both', expand=True)
 
-#Добавим кнопку на первый фрейм
-button1 = tk.Button(frame1, text='Построить гамильтонов цикл', command=lambda: nearest_neighbor_hamiltonian_cycle(G1))
+button1 = tk.Button(frame1, text='Построить гамильтонов цикл', command=lambda: nearest_neighbor_hamiltonian_cycle(G1, canvas2, pos))
 button1.pack(fill='both')
 
-#Добавим текстовое поле для вывода информации о гамильтоновом цикле на второй фрейм
 text1 = tk.Text(frame2, height=10, width=30)
 text1.pack(fill='both', expand=True)
 
